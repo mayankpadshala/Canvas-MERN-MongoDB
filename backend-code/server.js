588d75d67b3d0491f997a360794192bd021a73f2
mayankpadshala <mayankpadshala3012@gmail.com>
2019-04-14 16:10:25 -0700

    Test Changes

--- backend-code/server.js
+++ backend-code/server.js
@@ -90,4 +90,7 @@ app.get('/', (req, res) => {res.send('Hello')})
 
 const port = process.env.PORT || 5000;
 
+
+module.exports = app;
+
 app.listen(port, ()=> {console.log(`Server running on port ${port}`)})
